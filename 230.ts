eec5266546f87668577523e17cfcaaba3a87eab6
IsraPerez98 <isra1998isra@gmail.com>
2023-10-02 20:56:26 -0300

    fix: 230

--- 230.ts
+++ 230.ts
@@ -12,17 +12,6 @@
  * }
  */
 
-class TreeNode {
-    val: number
-    left: TreeNode | null
-    right: TreeNode | null
-    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-        this.val = (val===undefined ? 0 : val)
-        this.left = (left===undefined ? null : left)
-        this.right = (right===undefined ? null : right)
-    }
-}
-
 /**
  * Recursively finds the kth smallest value in a binary search tree.
  * @param {TreeNode | null} current - The current node in the binary search tree.
